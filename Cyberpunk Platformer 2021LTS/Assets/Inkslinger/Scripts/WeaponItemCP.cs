16306e69aee72c6162d43c3c409ac7d8da2577b1
Inkslinger <bedardyl10@hotmail.com>
2023-06-25 17:35:41 -0400

    Shifted from using base Script (WeaponItem)  TO My custom one (WeaponItemCP)

---
+++ Cyberpunk Platformer 2021LTS/Assets/Inkslinger/Scripts/WeaponItemCP.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Tools;
+using MoreMountains.InventoryEngine;
+
+
+[CreateAssetMenu(fileName = "New WeaponItem", menuName = "Custom/WeaponItem")]
+public class WeaponItemCP : WeaponItem
+{
+   
+
+
+
+
+
+
+}
