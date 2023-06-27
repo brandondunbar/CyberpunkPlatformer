ca6ad07d667326689fb13e53db5196d65b550aff
Inkslinger <bedardyl10@hotmail.com>
2023-06-26 21:40:44 -0400

    Changed sprite to test simplicity; Created new Weapon Class "ProjectileWeaponCP" which inherits from Base CorgiEngine "ProjectileEngine" Class.

---
+++ Cyberpunk Platformer 2021LTS/Assets/Inkslinger/Scripts/ProjectileWeaponCP.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Tools;
+using System;
+using MoreMountains.CorgiEngine;
+
+public class ProjectileWeaponCP : MonoBehaviour
+{
+  
+}
