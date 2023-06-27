b3570f5113816798fdca64be5cbc57614257fb9e
Inkslinger <bedardyl10@hotmail.com>
2023-06-27 16:09:16 -0400

    A somewhat working knockback,  needs some polishing

--- Cyberpunk Platformer 2021LTS/Assets/Inkslinger/Scripts/ProjectileWeaponCP.cs
+++ Cyberpunk Platformer 2021LTS/Assets/Inkslinger/Scripts/ProjectileWeaponCP.cs
@@ -5,7 +5,51 @@ using MoreMountains.Tools;
 using System;
 using MoreMountains.CorgiEngine;
 
-public class ProjectileWeaponCP : MonoBehaviour
+public class ProjectileWeaponCP : ProjectileWeapon
 {
-  
+
+
+
+    // Override the SpawnProjectile method
+    public override GameObject SpawnProjectile(Vector3 spawnPosition, int projectileIndex, int totalProjectiles, bool triggerObjectActivation = true)
+    {
+        GameObject projectile = base.SpawnProjectile(spawnPosition, projectileIndex, totalProjectiles, triggerObjectActivation);
+
+        // Apply knockback to the player
+        if (Owner != null)
+        {
+            Vector3 knockbackDirection = spawnPosition - Owner.transform.position;
+            Vector3 normalizedKnockbackDirection = -knockbackDirection.normalized; // Multiply by -1 to invert the direction
+            float knockbackDistance = 2f; // Adjust this value to control the knockback distance
+            float knockbackSpeed = 10f; // Adjust this value to control the speed of the knockback
+
+            RaycastHit hit;
+            if (Physics.Raycast(Owner.transform.position, normalizedKnockbackDirection, out hit, Mathf.Abs(knockbackDistance)))
+            {
+                // If the raycast hits a collider, adjust the knockback distance
+                knockbackDistance = -hit.distance;
+            }
+
+            StartCoroutine(PerformKnockback(normalizedKnockbackDirection, knockbackDistance, knockbackSpeed));
+        }
+
+        return projectile;
+    }
+
+
+
+
+    //Generated from chatgpt- makes the movement smoother
+    private IEnumerator PerformKnockback(Vector3 direction, float distance, float speed)
+    {
+        float remainingDistance = Mathf.Abs(distance);
+        while (remainingDistance > 0f)
+        {
+            float moveAmount = Mathf.Min(remainingDistance, speed * Time.deltaTime);
+            Owner.transform.Translate(direction * moveAmount);
+            remainingDistance -= moveAmount;
+            yield return null;
+        }
+    }
+
 }
